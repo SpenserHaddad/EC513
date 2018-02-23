efe89f32d738b94d5dc8bba9e5be672efd2b09dc
Spenser Haddad <spenserh@bu.edu>
2018-02-23 14:24:23 -0500

    Adding directory support to printing script
    print_results.py can now see if the input argument is a directory, and search it for all results files and print their individual and total results

--- lab2/print_results.py
+++ lab2/print_results.py
@@ -1,29 +1,56 @@
 #!/usr/bin/python3
+from pathlib import Path
 import sys
 import re
 
-if len(sys.argv) > 1:
-	result_file = sys.argv[1]
-else:
-	result_file = 'result.out'
-
-with open(result_file) as f:
-	result_text = f.read()
-
 pattern = re.compile(r'takenCorrect: (\d+)  takenIncorrect: (\d+) notTakenCorrect: (\d+) notTakenIncorrect: (\d+)')
 
-m = pattern.match(result_text)
-if not m:
-	raise ValueError("Invalid results file")
 
-results = [int(g) for g in m.groups()]
-total_branches = sum(results)
-total_correct = results[0] + results[2]
-correct_rate = 100 * (total_correct / total_branches)
+def read_results_file(result_file):
+	with open(result_file) as f:
+		result_text = f.read()
+	
+	m = pattern.match(result_text)
+	if not m:
+		raise ValueError('Invalid results file')
 
-print(f'Taken Correct = {results[0]}')
-print(f'Taken Incorrect = {results[1]}')
-print(f'Not Taken Correct = {results[2]}')
-print(f'Not Taken Incorrect = {results[3]}')
-print(f'\nResults: {total_correct} / {total_branches} ({correct_rate:0.3f}%)')
+	return [int(g) for g in m.groups()]
+	
+
+def print_results(results, title):
+
+	total_branches = sum(results)
+	total_correct = results[0] + results[2]
+	correct_rate = 100 * (total_correct / total_branches)
+
+	print(f'{title}:')
+	print(f'\tTaken Correct = {results[0]}')
+	print(f'\tTaken Incorrect = {results[1]}')
+	print(f'\tNot Taken Correct = {results[2]}')
+	print(f'\tNot Taken Incorrect = {results[3]}')
+	print(f'\n\tResults: {total_correct} / {total_branches} ({correct_rate:0.3f}%)\n')
+
+
+if __name__ == '__main__':
+	if len(sys.argv) == 1:
+		results = read_results_file('result.out')
+		print_results(results, 'result.out')
+		sys.exit()	
+	
+	result_path = Path(sys.argv[1])
+	if result_path.is_dir():
+		total_results = [0, 0, 0, 0]
+		for result_file in result_path.glob('**/*.txt.out'):
+			short_name = result_file.name.split('_')[0]
+			results = read_results_file(result_file)
+			print_results(results, short_name)
+
+			total_results = [t + r for t, r in zip(total_results, results)]
+
+		print_results(total_results, 'Total')
+
+	else:
+		short_name = result_path.name.split('_')[0]
+		results = read_results_file(result_path)
+		print_results(results, short_name)
 
